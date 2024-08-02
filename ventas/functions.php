f65fbae3edb5cfa025de2a112f811b6e127d791a
alozano-81 <yeisonandreslozano@gmail.com>
2024-08-02 14:05:42 -0500

    Update functions.php
    probar openshift varialbe en db

--- ventas/functions.php
+++ ventas/functions.php
@@ -1,9 +1,9 @@
 <?php
 function pdo_connect_mysql() {
     $DATABASE_HOST = 'localhost';
-    $DATABASE_USER = 'root';
-    $DATABASE_PASS = '';
-    $DATABASE_NAME = 'tienda';
+    $DATABASE_USER = 'userQ8W';
+    $DATABASE_PASS = 'ksXDBfFQJbxXPosh';
+    $DATABASE_NAME = 'sampledb';
     try {
     	return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
     } catch (PDOException $exception) {
